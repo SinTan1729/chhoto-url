cf4d7a849644303e89c34257918d3f7259b01872
Przemek Dragańczuk <draganczukp@gmail.com>
2020-02-14 18:40:14 +0100

    Added static file configuration to main

--- src/main/java/tk/draganczuk/url/App.java
+++ src/main/java/tk/draganczuk/url/App.java
@@ -4,9 +4,24 @@ import static spark.Spark.*;
 
 public class App {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		// Useful for developing the frontend
+		// http://sparkjava.com/documentation#examples-and-faq -> How do I enable automatic refresh of static files?
+		if (System.getenv("dev").equals("true")) {
+			String projectDir = System.getProperty("user.dir");
+			String staticDir = "/src/main/resources/public";
+			staticFiles.externalLocation(projectDir + staticDir);
+		} else {
+			staticFiles.location("/public");
+		}
+
+		get("/", (req, res) -> {
+			res.redirect("/index.html");
+			return "Redirect";
+		});
+
 		get("/all", Routes::getAll);
 		post("/new", Routes::addUrl);
 		get("/:shortUrl", Routes::goToLongUrl);
-    }
+	}
 }
