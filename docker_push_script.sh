9fdb634d71bd6c231f4fa34507a2a75aae3625fb
SinTan1729 <sayantan.santra689@gmail.com>
2024-03-07 14:30:41 -0600

    new: Added a convenience script for publishing

---
+++ docker_push_script.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+
+v_patch=$(cat actix/Cargo.toml | sed -rn 's/^version = "(.+)"$/\1/p')
+v_minor=$(echo $v_patch | sed -rn 's/^(.+\..+)\..+$/\1/p')
+v_major=$(echo $v_minor | sed -rn 's/^(.+)\..+$/\1/p')
+
+name="chhoto-url"
+
+docker build -t $name .
+
+for tag in latest $v_patch $v_minor $v_major;
+do
+    docker tag $name sintan1729/$name:$tag
+done
+
+echo "Do you want to push these to Docker Hub?"
+select yn in "Yes" "No";
+do
+    if [ "$yn" = "Yes" ]; then
+        for tag in latest $v_patch $v_minor $v_major;
+        do
+            docker push sintan1729/$name:$tag
+        done
+    else
+        echo "Okay! Not pushing."
+    fi
+    break
+done
+
