6d3d220cff74027ebd1cd7928737a4984e9d088d
SinTan1729 <sayantan.santra689@gmail.com>
2024-03-25 01:26:13 -0500

    build: Fix the docker_push_script

--- docker_push_script.sh
+++ docker_push_script.sh
@@ -7,9 +7,10 @@ elif [ "$1" == "release" ]; then
     v_patch=$(cat actix/Cargo.toml | sed -rn 's/^version = "(.+)"$/\1/p')
     v_minor=$(echo $v_patch | sed -rn 's/^(.+\..+)\..+$/\1/p')
     v_major=$(echo $v_minor | sed -rn 's/^(.+)\..+$/\1/p')
-
+    
+    make build
     name="chhoto-url"
     docker buildx build --push --tag sintan1729/$name:$v_major --tag sintan1729/$v_minor: \
-        --tag sintan1729/$name:$v_patch --tag sintan1729/$name:latest --platform linux/amd64,linux/arm64
+        --tag sintan1729/$name:$v_patch --tag sintan1729/$name:latest --platform linux/amd64,linux/arm64,linux/arm/v7 -f Dockerfile.multiarch .
 fi
 
