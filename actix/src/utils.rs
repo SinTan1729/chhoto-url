599b013fc997d3c0605f4c0853528f8b6303bb14
SinTan1729 <sayantan.santra689@gmail.com>
2024-03-31 00:51:32 -0500

    chg: Change uid to UID

--- actix/src/utils.rs
+++ actix/src/utils.rs
@@ -99,7 +99,7 @@ fn gen_link(style: String, len: usize) -> String {
     static CHARS: [char; 36] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
         'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    if style == "uid" {
+    if style == "UID" {
         nanoid!(len, &CHARS)
     } else {
         format!(
