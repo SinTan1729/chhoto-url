d39bd8a46a41ee98a77acd1c2b4805cf219d1571
SinTan1729 <sayantan.santra689@gmail.com>
2025-05-05 19:35:01 -0500

    fix: Errors when doing the migration

--- actix/src/database.rs
+++ actix/src/database.rs
@@ -15,7 +15,7 @@ pub struct DBRow {
 // Find a single URL
 pub fn find_url(shortlink: &str, db: &Connection, needhits: bool) -> (Option<String>, Option<i64>) {
     let query = if needhits {
-        "SELECT long_url,hits FROM urls WHERE short_url = ?1"
+        "SELECT long_url, hits FROM urls WHERE short_url = ?1"
     } else {
         "SELECT long_url FROM urls WHERE short_url = ?1"
     };
@@ -85,10 +85,8 @@ pub fn delete_link(shortlink: String, db: &Connection) -> bool {
 
 // Open the DB, and create schema if missing
 pub fn open_db(path: String) -> Connection {
-    // Current user version, should be incremented on schema change
-    let user_version = 1;
-
     let db = Connection::open(path).expect("Unable to open database!");
+
     // Create table if it doesn't exist
     db.execute(
         "CREATE TABLE IF NOT EXISTS urls (
@@ -96,10 +94,9 @@ pub fn open_db(path: String) -> Connection {
             long_url TEXT NOT NULL,
             short_url TEXT NOT NULL,
             hits INTEGER NOT NULL
-            expiry_time INTEGER,
-            );
-        PRAGMA schema.user_version = ?1",
-        [&user_version],
+            )",
+        // expiry_time is added later during migration 1
+        [],
     )
     .expect("Unable to initialize empty database.");
 
@@ -110,19 +107,21 @@ pub fn open_db(path: String) -> Connection {
     )
     .expect("Unable to create index on short_url.");
 
-    let current_user_version = db
-        .execute("SELECT user_version FROM pragma_user_version", [])
-        .expect("Unable to read pragma_user_version.");
+    let current_user_version: u32 = db
+        .query_row_and_then("SELECT user_version FROM pragma_user_version", [], |row| {
+            row.get(0)
+        })
+        .unwrap_or_default();
 
     // Migration 1: Add expiry_time, introduced in 5.9.0
     if current_user_version < 1 {
-        db.execute(
-            "ALTER TABLE urls ADD COLUMN expiry_time;
-                    PRAGMA schema.user_version = 1",
-            [],
-        )
-        .expect("Unable to apply migration 1.");
+        db.execute("ALTER TABLE urls ADD COLUMN expiry_time INTEGER", [])
+            .expect("Unable to apply migration 1.");
     }
+
+    // Set current user_version. Should be incremented on change of schema.
+    db.execute("PRAGMA user_version = 1", [])
+        .expect("Unable to set user_version.");
 
     db
 }
