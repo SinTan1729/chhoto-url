506f21d6e013fdceb7438a94c9f4675b7bb95b51
SinTan1729 <sayantan.santra689@gmail.com>
2026-07-01 04:10:18 -0500

    test: Diambiguate tokio and std sleep

--- backend/src/tests/access.rs
+++ backend/src/tests/access.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 use actix_web::test;
-use std::{thread::sleep, time::Duration};
+use std::time::Duration;
 
 use super::utils::*;
 
@@ -102,7 +102,7 @@ async fn link_expiry() {
     let (status, _) = add_link(&app, &api_key, "test1", 1, "").await;
     assert!(status.is_success());
     let one_second = Duration::from_secs(1);
-    sleep(one_second);
+    std::thread::sleep(one_second);
 
     let req = test::TestRequest::get().uri("/test1").to_request();
     let resp = test::call_service(&app, req).await;
@@ -164,7 +164,7 @@ async fn edit_expiry() {
     assert_eq!(reply.expiry_time, now + 1);
 
     let one_second = Duration::from_secs(1);
-    sleep(one_second);
+    std::thread::sleep(one_second);
     let status = edit_link(&app, &api_key, "test1", true, None, None).await;
     assert!(status.is_client_error());
 }
